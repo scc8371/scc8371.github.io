79d9fa0f62be318eb833a0e295056406d168abca
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2024-03-02 17:57:12 -0500

    more changes

--- src/popoutWords.js
+++ src/popoutWords.js
@@ -44,6 +44,21 @@ let template3 = `
 </style>
 `
 
+let template4 = `
+<span class='lead-programmer'>Lead Programmer</span>
+
+<style>
+    .lead-programmer{
+        color: #e9e9e9;
+        font-size: 1.35rem;
+
+        font-family: 'Lalezar', cursive;
+            text-shadow: 2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000,
+        1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000;
+    }
+</style>
+`
+
 class GameDeveloper extends HTMLElement {
     constructor() {
         super();
@@ -71,8 +86,18 @@ class UIDeveloper extends HTMLElement {
     }
 }
 
+class LeadProgrammer extends HTMLElement{
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML = template4;
+    }
+}
+
 customElements.define("game-developer", GameDeveloper);
 customElements.define("software-developer", SoftwareDeveloper);
 customElements.define("ui-designer", UIDeveloper);
+customElements.define("lead-programmer", LeadProgrammer);
 
-export { GameDeveloper, SoftwareDeveloper, UIDeveloper };
+export { GameDeveloper, SoftwareDeveloper, UIDeveloper, LeadProgrammer };
