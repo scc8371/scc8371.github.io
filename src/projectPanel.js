e54fba6815698ec5ee05cb2f8423f53f27a32b99
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2026-03-09 14:49:51 -0400

    improve loading anim

--- src/projectPanel.js
+++ src/projectPanel.js
@@ -12,7 +12,7 @@ const template = `
 
 <div class="project-panel">
 <div class="card">
-  <img src="media/bottle-knights.png" class="card-img-top project-image" width="400" height="300" alt="Default">
+  <img src="media/bottle-knights.png" class="card-img-top project-image" width="400" height="300">
   <img src="media/def-icon.png" class='sm-icon'>
   <div class="card-body">   
   
